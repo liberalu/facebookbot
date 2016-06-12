e7dde383e8ab6360e3582af14c793ae8d8384360
Evaldas Kazlauskas <kazlauskas.evaldas@gmail.com>
2016-06-12 23:34:18 +0300

    Added functiom toArray in Message class

--- src/FacebookBot/Callbacks/Message.php
+++ src/FacebookBot/Callbacks/Message.php
@@ -183,4 +183,16 @@ class Message
     }
 
 
+    /**
+     * To array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+
+        return get_object_vars($this);
+    }
+
+
 }
